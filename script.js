cbfff530c5ac438ffb2d5302c09b02def766cfd0
YOUR <YOUR>
2026-06-15 13:32:33 -0400

    finished Project(Pokemon)

--- script.js
+++ script.js
@@ -15,10 +15,15 @@ let optionOneBtn = document.querySelector('.option-one');
 let optionTwoBtn = document.querySelector('.option-two');
 let optionThreeBtn = document.querySelector('.option-three')
 let optionFourBtn = document.querySelector('.option-four')
+let optionFiveBtn = document.querySelector('.option-five')
+let optionSixBtn = document.querySelector('.option-six')
 
 let continueBtnScreen = document.querySelector('.continue-button-screen')
+let continueBtnScreen2 = document.querySelector('.continue-button-screen2')
 let optionOneScreen = document.querySelector('.option-one-screen');
 let optionTwoScreen = document.querySelector('.option-two-screen')
+let optionEnd = document.querySelector('.option-one-end')
+let optionEnd2 = document.querySelector('.option-two-end')
 let opening = document.querySelector('.story-opening');
 optionOneBtn.addEventListener('click', function(){
 
@@ -39,11 +44,23 @@ optionTwoBtn.style.display = 'none'
 optionThreeBtn.addEventListener('click',function(){
   optionOneScreen.style.display = 'none';
 continueBtnScreen.style.display = 'block';
-})
+});
 optionFourBtn.addEventListener('click', function(){
     optionTwoScreen.style.display = 'none';
-    continueBtnScreen.style.display='block';
+    continueBtnScreen2.style.display='block';
+});
+optionFiveBtn.addEventListener('click', function(){
+    optionEnd.style.display= 'block';
+    continueBtnScreen.style.display ='none';
+});
+optionSixBtn.addEventListener('click', function(){
+    optionEnd2.style.display = 'block';
+    continueBtnScreen2.style.display = 'none';
+
+
+
 })
+
 
 
 
