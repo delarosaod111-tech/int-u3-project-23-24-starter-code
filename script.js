0f05dcfcc663e9274a087524cdb90b11b6fddcb5
YOUR <YOUR>
2026-06-26 10:22:20 -0400

    hey101

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-// Declare variables below to save the different divs of your story.
+let AboutMeBtn = document.querySelector('#About-me');
+let ProjectsBtn = document.querySelector('#Projects');
+let ExperiencesBtn = document. querySelector('#Experiences');
+let ContactInfoBtn = document.querySelector('#Contact-Info');
+let AboutMeBtn2 = document.querySelector('#About-me2');
+let ProjectsBtn2 = document.querySelector('#Projects2');
 
 
 
+document.querySelector('#About-me').addEventListener('click', function(){
+    window.location.href = "AboutMe.html";
+})
 
+document.querySelector('#Projects').addEventListener('click', function(){
+    window.location.href = "Projects.html";
+})
 
+document.querySelector('#Experiences').addEventListener('click', function(){
+    window.location.href = "Experiences.html";
+})
 
+document.querySelector('#Contact-Info').addEventListener('click', function(){
+    window.location.href = "ContactInfo.html";
+})
+document.querySelector('#About-me2').addEventListener('click', function(){
+    window.location.href = "AboutMe.html";
+})
+document.querySelector('#Projects2').addEventListener('click', function(){
+    window.location.href = "Projects.html";
+})
